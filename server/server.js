baa1c66e801d7b8c14666f717065d6f4567d59c3
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-07-20 01:38:37 +0800

    chore: create a server directory

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import cors from 'cors'
+import Chance from 'chance'
+
+const chance = new Chance();
+const app = express();
+
+const animals = [...Array(250).keys()].map(id => {
+  return {
+    id,
+    type: chance.animal(),
+    age: chance.age(),
+    name: chance.name(),
+  }
+})
+
+app.get('', (req, res) => {
+  const q = req.query.q?.toLowerCase() || '';
+  console.log(q);
+  const results = animals.filter(animal => animal.type.toLowerCase().includes(q));
+
+  res.send(results);
+})
+
+app.listen(8080, () =>{
+  console.log('Listening to port http://localhost:8080');
+});
+
+
+app.use(cors());
+app.use(express.json());
